01871e7692f25648e7d4ee504b50c29b23ce3a13
Yugen <yugensochi89@gmail.com>
2023-08-13 21:15:33 +0300

    first commit

---
+++ WebApplication2/Models/GoodsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class GoodsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public object Price { get; set; }
+    }
+}
---
+++ WebApplication2/Models/Repository/EFDBContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication2.Models.Repository
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<GoodsModel> Goods { get; set; }
+    }
+}
